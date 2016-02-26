1bbfd5750e2c4f54df1c0bef02f1c2d17bd6310d
Jordan Meier <jbirdmeier@gmail.com>
2016-02-26 08:54:23 -0800

    add methods and pass tests for save, getAll and delete methods in Stylist class

--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -1,5 +1,73 @@
 <?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
 
+    require_once "src/Stylist.php";
+    require_once "src/Client.php";
 
+    $server = 'mysql:host=localhost;dbname=hair_salon_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
 
+    class StylistTest extends PHPUnit_Framework_TestCase
+    {
+
+        protected function tearDown()
+        {
+          Stylist::deleteAll();
+        //   Client::deleteAll();
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $name = "Shirley";
+            $test_Stylist = new Stylist($name);
+            $test_Stylist->save();
+
+            //Act
+            $result = Stylist::getAll();
+
+            //Assert
+            $this->assertEquals($test_Stylist, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $name = "Shirley";
+            $name2 = "Bertha";
+            $test_Stylist = new Stylist($name);
+            $test_Stylist->save();
+            $test_Stylist2 = new Stylist($name2);
+            $test_Stylist2->save();
+
+            //Act
+            $result = Stylist::getAll();
+
+            //Assert
+            $this->assertEquals([$test_Stylist, $test_Stylist2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $name = "Shirley";
+            $name2 = "Bertha";
+            $test_Stylist = new Stylist($name);
+            $test_Stylist->save();
+            $test_Stylist2 = new Stylist($name2);
+            $test_Stylist2->save();
+
+            //Act
+            Stylist::deleteAll();
+            $result = Stylist::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
+        }
+    }
 ?>
