c99a1c5d608be514c3c08175b2ff787d74ea73a4
Jordan Meier <jbirdmeier@gmail.com>
2016-02-26 09:26:21 -0800

    add save, deleteAll and getAll methods to Client class

--- src/Client.php
+++ src/Client.php
@@ -49,7 +49,32 @@
             return $this->stylist_id;
         }
 
-    }
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO clients (name, phone, stylist_id) VALUES ('{$this->getName()}', '{$this->getPhone()}', {$this->getStylistId()});");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
 
+        static function getAll()
+        {
+            $returned_clients = $GLOBALS['DB']->query("SELECT * FROM  clients;");
+            $clients = array();
+            foreach($returned_clients as $client) {
+                $name = $client['name'];
+                $id = $client['id'];
+                $phone = $client['phone'];
+                $stylist_id = $client['stylist_id'];
+                $new_client = new Client($id, $name, $phone, $stylist_id);
+                array_push($clients, $new_client);
+            }
+            return $clients;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM clients;");
+        }
+
+    }
 
 ?>
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,4 +1,103 @@
-<?php 
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+    require_once "src/Client.php";
+    require_once "src/Stylist.php";
 
+    $server = 'mysql:host=localhost;dbname=hair_salon_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+
+    class ClientTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Client::deleteAll();
+            Stylist::deleteAll();
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $name = "Shirley";
+            $id = null;
+            $test_stylist = new Stylist($name, $id);
+            $test_stylist->save();
+
+
+            $name = "Barbara";
+            $phone = "123-456-7899";
+            $stylist_id = $test_stylist->getId();
+            $test_client = new Client($id = null, $name, $phone, $stylist_id);
+            $test_client->save();
+
+            //Act
+            $result = Client::getAll();
+            //Assert
+            $this->assertEquals($test_client, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $name = "Shirley";
+            $id = null;
+            $test_stylist = new Stylist($name, $id);
+            $test_stylist->save();
+
+            $name = "Barbara";
+            $phone = "123-456-7899";
+            $stylist_id = $test_stylist->getId();
+            $test_client = new Client($id = null, $name, $phone, $stylist_id);
+            $test_client->save();
+
+
+            $name2 = "Buffy";
+            $phone2 = "126-456-7777";
+            $stylist_id2 = $test_stylist->getId();
+            $test_client2 = new Client($id2 = null, $name2, $phone2, $stylist_id2);
+            $test_client2->save();
+
+
+            //Act
+            $result = Client::getAll();
+
+            //Assert
+            $this->assertEquals([$test_client, $test_client2], $result);
+        }
+
+        function test_deleteAll()
+    {
+        //Arrange
+        $name = "Shirley";
+        $id = null;
+        $test_stylist = new Stylist($name, $id);
+        $test_stylist->save();
+
+        $name = "Barbara";
+        $phone = "123-456-7899";
+        $stylist_id = $test_stylist->getId();
+        $test_client = new Client($id = null, $name, $phone, $stylist_id);
+        $test_client->save();
+
+
+        $name2 = "Buffy";
+        $phone2 = "126-456-7777";
+        $stylist_id2 = $test_stylist->getId();
+        $test_client2 = new Client($id2 = null, $name2, $phone2, $stylist_id2);
+        $test_client2->save();
+
+        //Act
+        Client::deleteAll();
+        $result = Client::getAll();
+
+        //Assert
+        $this->assertEquals([], $result);
+    }
+    }
 
 ?>
